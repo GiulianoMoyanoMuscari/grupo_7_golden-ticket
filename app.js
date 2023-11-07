6366dbe4e4b269d73ff59ff9434e6aa176399570
Jhonatan Abdala <jhonatan.jamir.abdala@gmail.com>
2023-11-07 16:21:28 -0300

    agregados endpoints y archivos html vacios

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const pathPublic = path.join(__dirname, './public/');
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './views/home.html'))
 });
+app.get("/product-details", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/productDetail.html"));
+});
+app.get("/product-cart", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/productCart.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
 
 // Dejamos Estaticos a los recursos de la carpeta Public //
 app.use(express.static(pathPublic))
