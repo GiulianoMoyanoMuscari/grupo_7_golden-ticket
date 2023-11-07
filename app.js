f3c286027ef53088e78079418cf90b328867c7ae
GiulianoMoyanoMuscari <moyanomuscarigiuliano@gmail.com>
2023-11-07 15:31:29 -0300

    Creada la estructura básica del proyecto

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+
+const PORT = 3000;
+
+const pathPublic = path.join(__dirname, './public/');
+
+// Levantamos el Servidor //
+app.listen(PORT, ()=> console.log(`Servidor funcionando en el puerto ${PORT}. http://localhost:3000/`));
+
+// Definimos Rutas y Flujo Request-Response  //
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+// Dejamos Estaticos a los recursos de la carpeta Public //
+app.use(express.static(pathPublic))
