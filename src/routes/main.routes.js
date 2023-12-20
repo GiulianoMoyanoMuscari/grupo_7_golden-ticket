c36c06c09976850ea632c132eaa4334302d00801
GiulianoMoyanoMuscari <moyanomuscarigiuliano@gmail.com>
2023-12-20 16:33:31 -0300

    fix rutas

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // ************ Controller Require ************
-const mainController = require('../controllers/main.controllers');
+const mainController = require('../controllers/main.controller');
 
 router.get('/', mainController.index); 
 
