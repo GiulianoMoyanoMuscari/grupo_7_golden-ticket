6f446f1bfc084242ae825a4f05101f5880e1884b
Jhonatan Abdala <jhonatan.jamir.abdala@gmail.com>
2023-12-26 11:48:57 -0300

    agregados endpoints products.controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -4,20 +4,23 @@ const controller = {
   // Todos los productos
   index: (req, res) => {
     const products = db.readAll("products");
-    res.render("products/products.ejs", { list: products, individualCss: "product-detail" },);
+    res.render("products/products.ejs", {
+      list: products,
+      individualCss: "product-detail",
+    });
   },
 
   // Detalle de 1 producto
   detail: (req, res) => {
     const id = parseInt(req.params.id);
-    const product = db.find("products", id);
+    const product = db.readOne("products", id);
     res.send(product);
     // res.render("products/product-detail", { individualCss: "product-detail" });
   },
 
   // carrito de compra
   cart: (req, res) => {
-    res.render("products/product-cart", { individualCss: undefined});
+    res.render("products/product-cart", { individualCss: undefined });
   },
 
   // Formulario de Compra
@@ -27,11 +30,29 @@ const controller = {
 
   // Formulario de Creacion
   create: (req, res) => {
+    db.create("products", {
+      id: parseInt(req.params.id),
+      nombre: req.params.nombre,
+      descripcion: req.params.descripcion,
+      precio: req.params.precio,
+      categoria: req.params.categoria,
+      imagen: req.params.imagen,
+      fecha: req.params.fecha,
+    });
     res.render("products/product-create-form", { individualCss: "login" });
   },
 
   // Formulario de Edicion
   edit: (req, res) => {
+    db.update("products", parseInt(req.params.id), {
+      id: parseInt(req.params.id),
+      nombre: req.params.nombre,
+      descripcion: req.params.descripcion,
+      precio: req.params.precio,
+      categoria: req.params.categoria,
+      imagen: req.params.imagen,
+      fecha: req.params.fecha,
+    });
     res.render("products/product-edit-form", { individualCss: "login" });
   },
 };
