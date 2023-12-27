db6673f3e307113ec4c14492c35f50f516a93945
GiulianoMoyanoMuscari <moyanomuscarigiuliano@gmail.com>
2023-12-27 15:15:27 -0300

    Eliminando archivos y rutas que no usamos y CRUD:Create

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -21,44 +21,47 @@ const controller = {
     });
   },
 
-  // carrito de compra
+/*   // carrito de compra
   cart: (req, res) => {
     res.render("products/product-cart", { individualCss: undefined });
-  },
+  }, */
 
   // Formulario de Compra
-  purchase: (req, res) => {
+/*   purchase: (req, res) => {
     res.render("products/purchase-form", { individualCss: "purchase-form" });
-  },
+  }, */
 
   // Formulario de Creacion
   create: (req, res) => {
     res.render("products/product-create-form", { individualCss: "login" });
   },
 
-  // Endpoint de Creacion
-  put: (req, res) => {
-    db.create("products", {
-      id: parseInt(req.params.id),
-      nombre: req.params.nombre,
-      descripcion: req.params.descripcion,
-      precio: req.params.precio,
-      categoria: req.params.categoria,
-      imagen: req.params.imagen,
-      fecha: req.params.fecha,
-    });
-    res.send("producto creado!");
+  store: (req, res) => {
+    // Obtenemos los campos del body
+    const newProduct = {
+			id: Date.now(),
+			...req.body,
+			image: 'daniRibba.png'
+		}
+		
+    // creamos el producto
+    db.create('products', newProduct)
+
+		// redireccionamos
+		res.redirect('/products')
   },
 
-  destroy: (req, res) => {
+
+
+/*   destroy: (req, res) => {
     db.delete("products", req.params.id);
     res.reditect('/');
-  },
+  }, */
 
   // Formulario de Edicion
   edit: (req, res) => {
     const producto = db.readOne("products", parseInt(req.params.id));
-    /* db.update("products", producto.id, {
+    db.update("products", producto.id, {
       id: parseInt(req.params.id),
       nombre: req.params.nombre,
       descripcion: req.params.descripcion,
@@ -66,7 +69,7 @@ const controller = {
       categoria: req.params.categoria,
       imagen: req.params.imagen,
       fecha: req.params.fecha,
-    }); */
+    }); 
     res.render("products/product-edit-form", {
       individualCss: "login",
       data: producto,
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,15 +5,26 @@ const router = express.Router();
 // ************ Controller Require ************
 const productController = require("../controllers/products.controller");
 
+// **** GET ALL PRODUCTS ***//
 router.get("/", productController.index); // todos los productos
-router.get("/create", productController.create); // formulario de creacion
-router.get("/cart", productController.cart); // carrito
-router.get("/purchase", productController.purchase); // formulario de compra
-router.get("/:id", productController.detail); // detalle de 1 producto
-router.get("/:id/edit", productController.edit); // formulario de edicion
 
+
+// **** CREATE ONE PRODUCT ***//
+router.get("/create", productController.create); // formulario de creacion
+router.post("/", productController.store); // acción de creación
+
+
+// **** GET ONE PRODUCTS ***//
+router.get("/:id", productController.detail); // detalle de 1 producto
+
+
+// **** EDIT ONE PRODUCT ***//
+router.get("/:id/edit", productController.edit); // formulario de edicion
+// router.put("?", productController.?); // formulario de edicion
+
+
+// **** DELETE ONE PRODUCT ***//
 // router.delete("/:id", productController.destroy);
 
-/* router.post("/:id", productController.create); */
 
 module.exports = router;
