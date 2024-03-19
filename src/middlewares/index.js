c955d98d64265d42bc523af124ea52218030c3e2
Jhonatan Abdala <you@e>
2024-03-19 13:47:32 -0300

    feat: add Multer middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,8 +1,10 @@
 const auth = require("./auth.middleware");
 const permission = require("./permission.middleware");
 const session = require("./session.middleware");
+const multer = require("./multer.middleware");
 module.exports = {
   auth,
   permission,
   session,
+  multer,
 };
