206b914d91dd55e256e46d4109d990e569572f98
Jhonatan Abdala <you@e>
2024-03-22 16:31:54 -0300

    fix env and app.js clean

--- app.js
+++ app.js
@@ -1,42 +1,29 @@
-// ************ Require's ************
 const express = require("express");
-const path = require("path");
 const listEndpoints = require("express-list-endpoints");
 const methodOverride = require("method-override");
 const middlewares = require("./src/middlewares");
 const { sequelize } = require("./src/database/models");
-
-// ************ express() - (don't touch)************
+const routes = require("./src/routes");
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const pathPublic = path.join(__dirname, "./src/public/");
 const app = express();
 
-// ************ Configuraciones ************
-// Dejamos Estaticos a los recursos de la carpeta Public //
-const pathPublic = path.join(__dirname, "./src/public/");
-app.use(express.static(pathPublic));
-
-// Motor de Vistas
 app.set("views", path.join(__dirname, "/src/views"));
 app.set("view engine", "ejs");
 
-// Capturar la información de un formulario en un Objeto Literal
+app.use(express.static(pathPublic));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.use(middlewares.session);
 app.use(middlewares.auth);
-
-// Usar metodos PUT y DELETE
-app.use(methodOverride("_method"));
-
-// ************ Route System require and use() ************
-const routes = require("./src/routes");
 
 app.use("/", routes.mainRoutes);
 app.use("/users", routes.userRoutes);
 app.use("/tickets", routes.ticketRoutes);
 app.use("/events", routes.eventRoutes);
 
-// ************ Levantamos el Servidor ************
-const PORT = 3000;
 app.listen(PORT, () => {
   sequelize.sync();
   console.log("Endpoints de la aplicación: ");
